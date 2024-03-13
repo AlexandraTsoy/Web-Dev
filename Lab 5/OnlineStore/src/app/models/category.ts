b615a3ad3ddd1ff3cb4209d60c287072c013c2e6
AlexandraTsoy <98633765+AlexandraTsoy@users.noreply.github.com>
2024-03-13 13:56:21 +0530

    Delete Lab 5/OnlineStore directory

--- Lab 5/OnlineStore/src/app/models/category.ts
+++
@@ -1,8 +0,0 @@
-import {Item} from "./item";
-
-export interface Category{
-  id: number;
-  name: string;
-  image_link: string;
-  items: Item[];
-}
--- Lab 5/OnlineStore/src/app/models/item.ts
+++
@@ -1,9 +0,0 @@
-export interface Item {
-  id: number;
-  image_link: string;
-  name: string;
-  description: string;
-  rating: number;
-  likes: number;
-  link: string;
-}
