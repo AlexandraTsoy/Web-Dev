dbbfa11da633208fda7915492784f6c8ddbdbf02
AlexandraTsoy <98633765+AlexandraTsoy@users.noreply.github.com>
2024-03-13 13:59:39 +0530

    Add files via upload

---
+++ Lab 3/ToDo App/script.js
@@ -0,0 +1,42 @@
+let toDoList = ['Do laboratory work for Web Development', 'Install the Google', 'Go to the library'];
+
+function fillTheList(){
+    let printedList = '';
+
+    for(let i = 0; i < toDoList.length; i++){
+        printedList +=
+            '<div class="item"><input id="check'+i+'" class="subitem" type="checkbox" onchange="taskDone('+i+')">' +
+            '<p class="subitem" id="task'+i+'">' + toDoList[i] + '</p>' +
+            '<button class="subitem delete_button" onclick="deleteTask('+i+')">DELETE</button></div>'
+    }
+
+    document.getElementById('list').innerHTML = printedList;
+}
+
+
+function addNewTask(){
+    const task = document.getElementById('new_task').value
+    if(task && task !== '') {
+        toDoList.push(document.getElementById('new_task').value);
+        document.getElementById('new_task').value = '';
+        fillTheList();
+    }else{
+        alert('Error! ')
+    }
+}
+
+
+function deleteTask(i){
+    toDoList.splice(i, 1);
+    fillTheList();
+}
+
+function taskDone(i){
+    // console.log(document.getElementById(`check${i}`) )
+    if( document.getElementById(`check${i}`).checked )
+        document.getElementById(`task${i}`).innerHTML = `<s>${toDoList[i]}</s>`;
+    else
+        document.getElementById(`task${i}`).innerHTML = `<span>${toDoList[i]}</span>`;
+}
+
+fillTheList();
